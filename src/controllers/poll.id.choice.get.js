47ea3fcc3b692d461880754c4f4d4d7a6ee3aed8
brunoprosa <bruno.prosa01@gmail.com>
2023-09-07 13:01:05 -0300

    feat: get poll id choices

--- src/controllers/poll.id.choice.get.js
+++ src/controllers/poll.id.choice.get.js
@@ -1,7 +1,18 @@
 import { db } from "../app.js"
 
 export async function pollIdChoiceGet(req, res){
+
+    const pollId = req.params.id
     
-    
+    try{
+
+        const choices = await db.collection('choices').find({pollId: new ObjectId( pollId )}).toArray()
+        if(choices.length === 0) return res.status(404).send('poll not found')
+
+        res.send( choices )
+
+    } catch(err){
+        return res.status(500).send(err.message);
+    }
 
 }
