ce0bc52ed8897fd8f0b5c1e9bf473f1687840778
brunoprosa <bruno.prosa01@gmail.com>
2023-09-07 13:16:22 -0300

    feat: get poll

--- src/controllers/poll.get.js
+++ src/controllers/poll.get.js
@@ -2,4 +2,13 @@ import { db } from "../app.js";
 
 export async function pollGet(req, res){
     
+    try{
+
+        const polls = await db.collection('polls').find().toArray()
+        res.send( polls )
+
+    } catch(err){
+        return res.status(500).send(err.message);
+    }
+
 }
